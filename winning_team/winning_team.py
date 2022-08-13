2096abe7a65be2c1c65a627e65dd8824fb941d4e
Tims Guynes <tims.m.guynes@gmail.com>
2022-08-13 00:23:46 -0700

    removed unused project

--- winning_team/winning_team.py
+++ winning_team/winning_team.py
@@ -18,18 +18,6 @@ def basketball_score():
     team_01_score = (team_01_3pt * 3 ) + (team_01_2pt * 2) + (team_01_1pt)
     team_02_score = (team_02_3pt * 3 ) + (team_02_2pt * 2) + (team_02_1pt)
 
-#    count = 0
-
-#    for score_b in b_list:
-#        if count < 3:
-#            team_01.append(score_b)
-#            count += 1
-#        else:
-#            team_02.append(score_b)
-#            count += 1
-
-    #team_01_score = (team_01[0] * 3) + (team_01[1] * 2) + (team_01[2])
-    #team_02_score = (team_02[0] * 3) + (team_02[1] * 2) + (team_02[2])
     
     if team_01_score > team_02_score:
         return "Apples Win: " + str(team_01_score)
@@ -37,12 +25,5 @@ def basketball_score():
         return "Banana's Win: " + str(team_02_score)
     elif team_01_score == team_02_score:
         return "It's a tie."
-
-    #team_01_score = (team_01[0] * 3) + (team_01[1] * 2) + (team_01[2])
-    #team_02_score = (team_02[0] * 3) + (team_02[1] * 2) + (team_02[2])
-
-test_case_01 = [5, 1, 3, 1, 1, 3]
-test_case_02 = [1, 1, 3, 5, 1, 3]
-test_case_03 = [1, 1, 1, 1, 1, 1]
 
 print(basketball_score())
